c20a132ca9392bbea697279c1e4d5eb5417c60b8
DAZRY <1123150198@global.ac.id>
2026-07-01 09:54:16 +0700

    fix: update IP backend ke 192.168.110.93 & tambah intent filter   dompetkampus://pay

--- pasar_malam/lib/core/constants/api_constants.dart
+++ pasar_malam/lib/core/constants/api_constants.dart
@@ -1,6 +1,6 @@
 class ApiConstants {
   // Ganti dengan URL backend kamu
-  static const String baseUrl = 'http://192.168.110.87:8080/v1';
+  static const String baseUrl = 'http://192.168.110.93:8080/v1';
 
   // Auth endpoints
   static const String verifyToken = '/auth/verify-token';
