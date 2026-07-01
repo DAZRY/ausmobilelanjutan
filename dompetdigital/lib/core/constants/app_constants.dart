d55d19ee607b1b2b9f40002bb739de28a9a68a22
Lendra <lendradzr77@gmail.com>
2026-07-01 23:05:46 +0700

    mengubah warna tema

--- dompetdigital/lib/core/constants/app_constants.dart
+++ dompetdigital/lib/core/constants/app_constants.dart
@@ -3,7 +3,7 @@ class AppConstants {
   static const String appVersion = '1.0.0';
 
   // API
-  static const String baseUrl = 'http://192.168.110.93:8080';
+  static const String baseUrl = 'http://192.168.1.8:8080';
   static const String apiVersion = '/v1';
   static const int connectTimeout = 30;
   static const int receiveTimeout = 60;
